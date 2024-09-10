2b460d7f248fccf405c67a0509ef040163095e5b
Raden Akbar Agung Gumilang <120640056+ivillop@users.noreply.github.com>
2024-09-10 23:23:52 +0700

    Delete dist directory

--- dist/src/enum.js
+++
@@ -1,6 +0,0 @@
-export var CustomerType;
-(function (CustomerType) {
-    CustomerType["PLATINUM"] = "PLATINUM";
-    CustomerType["REGULAR"] = "REGULAR";
-    CustomerType["GOLD"] = "GOLD";
-})(CustomerType || (CustomerType = {}));
--- dist/tests/say-hello.test.js
+++
@@ -1,6 +0,0 @@
-import { sayHello } from "../src/say-hello";
-describe('sayHello', function () {
-    it('should return hello raden', function () {
-        expect(sayHello('raden')).toBe('Hello raden');
-    });
-});
